1a70d2be2b4a030037040f9139cb4da2f11fc3ce
Ника <@DESKTOP-BMCHS5M>
2024-03-28 21:42:59 +0300

    Random

--- Binary/main.cpp
+++ Binary/main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
 
 void main()
 {
 	setlocale (LC_ALL, "");
-	int i = 0; //номер разряда
-	for (; decimal; decimal /= 2; bynary[i++] = decimal % 2);
+	const int MAX_HEX_CAPACITY = 8;
+	char hex[MAX_HEX_CAPACITY] = {};
+	int decimal;
+	int i = 0; 	//номер разряда
+	
+	for (; decimal; decimal /= 2) bynary[i++] = decimal % 2);
 	//для получения 
 
 
---
+++ Random/main.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+#define tab "\t"
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int n = 5;
+	int arr[n];
+	int minRand, maxRand;
+	cout << "введиет минимальное случайное число:"; cin >> minRand;
+	cout << "введиет максимальное случайное число:"; cin >> maxRand;
+
+	//заполнение массива случайными числами
+	for (int i = 0; i < n; i++)
+	{
+		//arr[i] = rand()%100+50;
+		arr[i] = rand() % (maxRand - minRand) + minRand;
+	}
+	//сортировка массива
+
+
+
+	for (int i = 0; i < n; i++)// счетчик 'i' выбирает элемент кот надо поместить в мин значение
+	{
+		for (int j = i+1; j < n; j++) // счетчик пребирает оставшиеся элементы 
+		{
+			//arr]i] -  выбранный элемент массива
+			//arr [j] -  перебираемый элемент массива
+			if (arr[j] < arr[i])
+			{
+				int buffer = arr[i];
+				arr[i] = arr[j];
+				arr[j] = buffer;
+			}
+		}
+	}
+
+
+
+
+	//ввывод  отсортированного массива на экран
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+}
